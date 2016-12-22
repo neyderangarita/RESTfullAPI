ae8007917c110454d3ea346de3f9386160c0bb6f
neyder angarita <neyder6101@hotmail.com>
2016-12-22 02:51:46 -0500

    dsfdsfsd

--- app/Http/Controllers/ColegioController.php
+++ app/Http/Controllers/ColegioController.php
@@ -34,7 +34,7 @@ class ColegioController extends Controller
         $result = \DB::select('select colegio.nombre as nombre_colegio, CAST(comentario.calificacion AS INT) as calificacion, colegio.latitud, colegio.longitud,comentario.mensaje, users.nombre as nombre_usuario 
                                 from comentario inner join colegio on colegio.id = comentario.colegio_id 
                                 inner join users on users.id = comentario.user_id
-                                where colegio.codigo= ?', $id);
+                                where colegio.codigo= ?', [$id]);
         /*
         if(!$result)
         {
